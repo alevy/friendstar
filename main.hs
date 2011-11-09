579d3d6ac9a6bd298db4d7a06b2a3c2416814975
Amit Levy <aalevy@gmail.com>
2011-11-09 08:23:09 -0800

    Mucking with profile view

--- main.hs
+++ main.hs
@@ -5,6 +5,7 @@ import Control.Monad.Trans
 import RoutedServer
 import qualified Data.ByteString.Char8 as S
 import Text.StringTemplate
+import Text.StringTemplate.GenericStandard
 
 import System.IO
 
@@ -16,12 +17,13 @@ routing = [ routeTop $ routeConst $ resp301 "/home",
                     routeMap apps
                   , routeFileSys mimeMap (dirRedir "/index.html") "public"
                   ]
-apps = [("profile", routeVar $ routeFn profileController)]
+apps = [("profiles", routeVar $ routeFn profileController)]
+
 
 profileController req = do
   let profileId = (head $ reqPathParams req)
   profile <- lift $ run $ findProfile (read $ S.unpack profileId)
   let template = getTemplate "views/profile.html"
-  let view = render $ setAttribute "id" profileId $
+  let view = render $ setAttribute "profile" profile $
           newSTMP template
   return $ mkHtmlResp stat200 $ view
