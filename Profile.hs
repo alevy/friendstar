ab85b2f2e15c802f16de1853204b5ee6f20fd438
Amit Levy <aalevy@gmail.com>
2011-11-08 22:00:21 -0800

    Changed profile data types to native vs bson string types

--- Profile.hs
+++ Profile.hs
@@ -6,27 +6,38 @@ module Profile where
 import Control.Applicative
 import Control.Monad.Trans
 import Data.Bson
+import qualified Data.ByteString.Char8 as S
+import Data.Data
+import qualified Data.Text as T
 import Data.Time.Clock
 import Data.Typeable
 import Data.Maybe
 import Database.MongoDB
 
+newtype FSObjectId = FSObjectId S.ByteString deriving (Show, Eq, Data, Typeable)
+
+fromObjectId :: ObjectId -> FSObjectId
+fromObjectId id = FSObjectId $ S.pack $ show id
+
+toObjectId :: FSObjectId -> ObjectId
+toObjectId (FSObjectId id) = read $ S.unpack id
+
 data FSPost = FSPost {
-  postAuthorId :: ObjectId,
+  postAuthorId :: FSObjectId,
   postTimestamp :: UTCTime,
-  postText :: UString
-} deriving (Show, Eq, Typeable)
+  postText :: T.Text
+} deriving (Show, Eq, Data, Typeable)
 
 data FSProfile = FSProfile {
-  profileId :: Maybe ObjectId,
-  firstName :: UString,
-  middleName :: UString,
-  lastName :: UString,
-  currentCity :: UString,
-  friends :: [ObjectId],
-  incomingFriendRequests :: [ObjectId],
+  profileId :: Maybe FSObjectId,
+  firstName :: String,
+  middleName :: String,
+  lastName :: String,
+  currentCity :: String,
+  friends :: [FSObjectId],
+  incomingFriendRequests :: [FSObjectId],
   posts :: [FSPost]
-} deriving (Show, Eq, Typeable)
+} deriving (Show, Eq, Data, Typeable)
 
 defaultFSProfile :: FSProfile
 defaultFSProfile = FSProfile {
@@ -35,38 +46,40 @@ defaultFSProfile = FSProfile {
 }
 
 instance Val FSProfile where
-  val profile = Doc [ "_id" =: profileId profile,
+  val profile = Doc [ "_id" =: (fmap toObjectId $ profileId profile),
                   "first_name" =: firstName profile,
                   "middle_name" =: middleName profile,
                   "last_name" =: lastName profile,
-                  "current_city" =: currentCity profile]
-                 -- "friends" =: friends profile,
-                 -- "incoming_friend_requests" := incomingFriendRequests profile,
-                 -- "posts" := posts profile] :: Document
+                  "current_city" =: currentCity profile,
+                  "friends" =: (fmap toObjectId $ friends profile),
+                  "friends" =: (fmap toObjectId $ friends profile),
+                  "incoming_friend_requests" =: (fmap toObjectId $ incomingFriendRequests profile),
+                  "posts" =: posts profile]
 
   cast' (Doc doc) = Just defaultFSProfile {
-    profileId = at "_id" doc,
+    profileId = fmap fromObjectId $ at "_id" doc,
     firstName = at "first_name" doc,
     middleName = atOrDefault "middle_name" doc "",
     lastName = at "last_name" doc,
     currentCity = atOrDefault "current_city" doc "",
-    friends = atOrDefault "friends" doc [],
-    incomingFriendRequests = atOrDefault "incoming_friend_requests" doc []}
-    --posts = val $ atOrDefault "posts" doc []}
+    friends = fmap fromObjectId $ atOrDefault "friends" doc [],
+    incomingFriendRequests = fmap fromObjectId $ atOrDefault "incoming_friend_requests" doc [],
+    posts = fmap toPost $ atOrDefault "posts" doc []}
+    where toPost post = fromJust $ cast' post
 
 
 atOrDefault :: Val v => Label -> Document -> v -> v
 atOrDefault key doc def = maybe def id (Database.MongoDB.lookup key doc)
 
 instance Val FSPost where
-  val post = Doc [ "author_id" =: postAuthorId post,
+  val post = Doc [ "author_id" =: (toObjectId $ postAuthorId post),
                "timestamp" =: postTimestamp post,
-               "text" =: postText post]
+               "text" =: (T.unpack $ postText post)]
 
   cast' (Doc doc) = Just FSPost {
-    postAuthorId = at "author_id" doc,
+    postAuthorId = fromObjectId $ at "author_id" doc,
     postTimestamp = at "timestamp" doc,
-    postText = at "text" doc}
+    postText = T.pack $ at "text" doc}
 
 findProfile :: MonadIO m => ObjectId -> Action m FSProfile
 findProfile id = do
@@ -81,7 +94,7 @@ saveProfile profile
       return profile
   | otherwise = do
       ObjId objId <- insert "profiles" $ doc
-      return profile { profileId = Just objId }
+      return profile { profileId = Just $ fromObjectId objId }
   where (Doc doc) = val profile
 
 
